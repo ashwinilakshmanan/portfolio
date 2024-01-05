76405dbd0943425e28d6f8442d86223a8f00ab0e
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2024-01-05 12:19:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from './App.module.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className={styles.App}>
+      <h1>App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
